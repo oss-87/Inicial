3d7fbc57505a4fca82c87b6cf94175e3b0f2651c
Oscar Rojas <oscar.rojas87@gmail.com>
2014-12-12 18:11:30 -0200

    Comentarios

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <QCoreApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    // Aqui agrego una pequeña pregunta
+    qDebug() << "Hola mundo";
 
     return a.exec();
 }
